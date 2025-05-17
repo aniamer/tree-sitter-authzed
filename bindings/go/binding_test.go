e8e864a91b0c1d3d15268a691f10153f830d54d3
aniamer <ani@soundtrack.io>
2025-05-17 10:24:32 +0200

    learn queries

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -2,6 +2,7 @@ package tree_sitter_authzed_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	tree_sitter_authzed "github.com/aniamer/tree-sitter-authzed/bindings/go"
@@ -9,33 +10,51 @@ import (
 )
 
 func TestCanLoadGrammar(t *testing.T) {
-	//schema, err := os.ReadFile("basic_authzed_schema.zed")
-	//if err != nil {
-	//	t.Fatalf("Failed to read file: %v", err)
-	//}
+	schema, err := os.ReadFile("basic_authzed_schema.zed")
+	if err != nil {
+		t.Fatalf("Failed to read file: %v", err)
+	}
 
 	parser := tree_sitter.NewParser()
 	defer parser.Close()
 	language := tree_sitter.NewLanguage(tree_sitter_authzed.Language())
 	if language == nil {
 		t.Errorf("Error loading authzed-parser grammar")
+		return
 	}
-
-	fmt.Printf("languge %v", language.Version())
-	err := parser.SetLanguage(language)
+	err = parser.SetLanguage(language)
 	if err != nil {
 		t.Error(err)
+		return
 	}
-	tree := parser.Parse([]byte(`
-		definition user {
-			relation friend: user
-			permission view = friend
-	}`), nil)
+	tree := parser.Parse(schema, nil)
 
 	if tree == nil {
 		t.Error("Couldn't parse schema")
+		return
 	}
 	defer tree.Close()
-	root := tree.RootNode()
-	fmt.Println(root.ToSexp())
+
+	rootNode := tree.RootNode()
+	fmt.Printf("%s\n", rootNode.ToSexp())
+
+	query, query_err := tree_sitter.NewQuery(language, `((definition) @identifier)`)
+	if query_err != nil {
+		t.Error(err)
+	}
+
+	defer query.Close()
+
+	qc := tree_sitter.NewQueryCursor()
+	defer qc.Close()
+
+	captures := qc.Captures(query, tree.RootNode(), schema)
+
+	for match, index := captures.Next(); match != nil; match, index = captures.Next() {
+		fmt.Printf(
+			"Capture %d: %s\n",
+			index,
+			match.Captures[index].Node.Utf8Text(schema),
+		)
+	}
 }
--- grammar.js
+++ grammar.js
@@ -1,3 +1,4 @@
+
 module.exports = grammar({
   name: "authzed",
 
